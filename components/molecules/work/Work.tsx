74bbca2ec2f05ac9cf261cd1f148d94a515c225c
silvano <silvano.norberti@gmail.com>
2025-02-15 22:32:38 +0100

    Add card sample

--- components/molecules/work/Work.tsx
+++ components/molecules/work/Work.tsx
@@ -1,9 +1,11 @@
-import { DefaultText } from "@/components/atoms/text/DefaultText";
-
 function Work() {
   return (
     <div className="h-screen flex items-center justify-center">
-      <DefaultText>Hello</DefaultText>
+      <div className=" h-full w-full p-8 flex gap-4 flex-wrap">
+        <div className="sm:flex-col items-center justify-center font-Inconsolata border-1 rounded-sm bg-yellow-400 h-fit min-h-40 min-w-40">
+          a
+        </div>
+      </div>
     </div>
   );
 }
